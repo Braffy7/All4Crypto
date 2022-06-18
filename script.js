aa2b58635be92d968e7292795d8057802dac06e2
Marcin <you@example.com>
2022-06-18 19:32:55 +0200

    working APIS(full table)

--- script.js
+++ script.js
@@ -1,51 +1,62 @@
 var baseUrl = "https://api.coinranking.com/v2/coins";
-var proxyUrl = "https://cors-anywhere.herokuapp.com/";
+var proxyUrl = "https://corsproxy.io/?";
 var apiKey = "coinranking7f186a64cb9582061ee9f9cc76f5cd9cf26064a0cf336549";
 var urlNBP = "http://api.nbp.pl/api/exchangerates/rates/A/USD/"
 
 
 // API for cryptocurrencies & function creating Name / Symbol / Market Cap / Value $
 
-function cryptoAPI() {
+Promise.all([
     fetch(`${proxyUrl}${baseUrl}`, { 
         method: 'GET',
         headers: {
         'Content-Type': 'application/json',
         'X-My-Custom-Header': `${apiKey}`,
         'Access-Control-Allow-Origin': "*"
-        }
-    }).then((response) => {
-        if(response.ok) {
-            response.json().then((json) => displayList(json) 
-            );
-        }
+        }}),
+    fetch(`${proxyUrl}${urlNBP}`, {
+        method: 'GET',
+        headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': "*"
+        }})
+    ]).then((responses) => {
+        return Promise.all(responses.map(function (response) {
+            return response.json();
+        }));
+    }).then((data) => {
+        displayList(data);
     }).catch((error) => {
         console.log(error)
-    })
+    });
 
 
-    function displayList(json) {
-        let coinsData = json.data.coins;
-
+    function displayList(data) {
+        let coinsData = data[0].data.coins;
+        console.log(coinsData);
         if (coinsData.length > 0) {
             var cryptoCoin = "";
         }
+        let valueUSD = data[1].rates[0].mid;
 
         coinsData.forEach((coin) => {
-        cryptoCoin += `<tr class = 'row'>`;
+        cryptoCoin += `<tr>`;
         cryptoCoin += `<td> ${coin.name} </td>`;
         cryptoCoin += `<td> ${coin.symbol} </td>`;
         cryptoCoin += `<td> $ ${(coin.marketCap)} </td>`;
-        
         if (coin.price < 0.95) {
-            cryptoCoin += `<td class = 'price'> $ ${parseFloat(coin.price).toFixed(8)} </td>`;
+            cryptoCoin += `<td> $ ${parseFloat(coin.price).toFixed(8)} </td>`;
+            cryptoCoin += `<td> PLN ${(parseFloat(coin.price)*parseFloat(valueUSD)).toFixed(8)} </td>`;
         } else {
-            cryptoCoin += `<td class = 'price'> $ ${parseFloat(coin.price).toFixed(2)} </td>`;
-        }});
-        document.getElementById("data").innerHTML = cryptoCoin;
-        console.log(json);
-    }};
+            cryptoCoin += `<td> $ ${parseFloat(coin.price).toFixed(2)} </td>`;
+            cryptoCoin += `<td> PLN ${(parseFloat(coin.price)*parseFloat(valueUSD)).toFixed(2)} </td>`;
+        };
+            cryptoCoin += `<td><form><input type="number" step="0.0000000001"><button type="submit">Add</button></form></td>`
+        });
 
+        document.getElementById("data").innerHTML = cryptoCoin;
+    };
+/*
 // API For NBP & function creating Value PLN / Amount
 
 function nbpAPI() {
@@ -95,3 +106,5 @@ new Promise((resolve) => {
         resolve(cryptoAPI())
     }, 0 );
 }).then(nbpAPI())
+
+*/
