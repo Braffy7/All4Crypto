108af3010649ae3857d1c0330c975b8c40e66ab5
Marcin <you@example.com>
2022-06-26 10:01:11 +0200

    properly added input amounts in console

--- script.js
+++ script.js
@@ -25,86 +25,47 @@ Promise.all([
             return response.json();
         }));
     }).then((data) => {
-        displayList(data);
+        Promise.all([
+            displayList(data), cryptoAmount()]);
     }).catch((error) => {
         console.log(error)
     });
 
 
-    function displayList(data) {
-        let coinsData = data[0].data.coins;
-        console.log(coinsData);
-        if (coinsData.length > 0) {
-            var cryptoCoin = "";
-        }
-        let valueUSD = data[1].rates[0].mid;
+function displayList(data) {
+    let coinsData = data[0].data.coins;
+    if (coinsData.length > 0) {
+        var cryptoCoin = "";
+    }
+    let valueUSD = data[1].rates[0].mid;
 
-        coinsData.forEach((coin) => {
-        cryptoCoin += `<tr>`;
-        cryptoCoin += `<td> ${coin.name} </td>`;
-        cryptoCoin += `<td> ${coin.symbol} </td>`;
-        cryptoCoin += `<td> $ ${(coin.marketCap)} </td>`;
-        if (coin.price < 0.95) {
-            cryptoCoin += `<td> $ ${parseFloat(coin.price).toFixed(8)} </td>`;
-            cryptoCoin += `<td> PLN ${(parseFloat(coin.price)*parseFloat(valueUSD)).toFixed(8)} </td>`;
-        } else {
-            cryptoCoin += `<td> $ ${parseFloat(coin.price).toFixed(2)} </td>`;
-            cryptoCoin += `<td> PLN ${(parseFloat(coin.price)*parseFloat(valueUSD)).toFixed(2)} </td>`;
-        };
-            cryptoCoin += `<td><form><input type="number" step="0.0000000001"><button type="submit">Add</button></form></td>`
-        });
-
-        document.getElementById("data").innerHTML = cryptoCoin;
+    coinsData.forEach((coin) => {
+    cryptoCoin += `<tr>`;
+    cryptoCoin += `<td> ${coin.name} </td>`;
+    cryptoCoin += `<td> ${coin.symbol} </td>`;
+    cryptoCoin += `<td> $ ${(coin.marketCap)} </td>`;
+    if (coin.price < 0.95) {
+        cryptoCoin += `<td> $ ${parseFloat(coin.price).toFixed(8)} </td>`;
+        cryptoCoin += `<td> PLN ${(parseFloat(coin.price)*parseFloat(valueUSD)).toFixed(8)} </td>`;
+    } else {
+        cryptoCoin += `<td> $ ${parseFloat(coin.price).toFixed(2)} </td>`;
+        cryptoCoin += `<td> PLN ${(parseFloat(coin.price)*parseFloat(valueUSD)).toFixed(2)} </td>`;
     };
-/*
-// API For NBP & function creating Value PLN / Amount
-
-function nbpAPI() {
-    fetch(`${proxyUrl}${urlNBP}`, {
-        method: 'GET',
-        headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': "*"
-        }
-    }).then((response) => {
-        if(response.ok) {
-            response.json().then((json) => calcPLN(json)
-            );
-        }
-    }).catch((error) => {
-        console.log(error)
+        cryptoCoin += `<td><form><input name="inputAmount" type="number" step="0.0000000001"><button type="submit">Add</button></form></td>`
     });
 
-    function calcPLN(json) {
-        console.log(json);
-        let valuePLN = parseFloat(json.rates[0].mid);
-        console.log(valuePLN)
+    document.getElementById("data").innerHTML = cryptoCoin;
+    
+};
 
-        const listRow = document.querySelectorAll(".row");
-        const arrayRow = Array.from(listRow)
-        console.log(arrayRow);
+function cryptoAmount() {
+    const table = document.querySelector('.list__table');
+    console.log(table);
+
+    table.addEventListener('submit', (event) => {
+        event.preventDefault();
+        item = event.target.inputAmount;
+        console.log(item.value);
         
-
-        let listPrice = document.querySelectorAll(".price");
-        let arrayPrice = Array.from(listPrice);
-        console.log(arrayPrice);
-        console.log(listPrice);
-
-        for(let i=0; i<arrayPrice.length; i++) {
-            console.log(i);
-            let calculatedPLN = 'PLN ' + (parseFloat(arrayPrice[i]) * valuePLN);
-            let th = document.createElement('th');
-            arrayRow[i].appendChild(th).innerHTML = calculatedPLN;
-            console.log(calculatedPLN);
-        };
-    }};
-
-// Sequence of functions
-
-new Promise((resolve) => {
-    setTimeout(() => {
-        resolve(cryptoAPI())
-    }, 0 );
-}).then(nbpAPI())
-
-*/
+});
+};
